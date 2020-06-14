8893f9da19dcff4b507a99134aa6a8e4f4047d7e
Cesar Frick <cfrick@publicisgroupe.net>
2020-06-14 01:26:29 -0500

    Move the Search Component to its own folder

--- components/select/Select.jsx
+++ components/select/Select.jsx
@@ -1,20 +1,21 @@
 import React, { useState } from 'react'
 
-export const Select = ({ defaultOptionText = 'Select...', onChange, options = [], name, addDefault = true }) => {
+export const Select = ({ defaultOptionText = 'Select...', onChange, options = [], name = '', addDefault = true }) => {
     const [inputValue, setInputValue] = useState('')
 
     const onUpdate = (event) => {
+        setInputValue(event.target.value)
+
         if (onChange) {
             onChange(event)
-        } else {
-            setInputValue(event.target.value)
         }
     }
 
     return (
         <select name={name} value={inputValue} onChange={onUpdate}>
             {addDefault && <option key="default" value="">{defaultOptionText}</option>}
-            {options.map(option => <option key={option.content}>{option.content}</option>)}
+            {options.map((option, index) =>
+                <option key={option.content || option} value={index}>{option.content || option}</option>)}
         </select>
     )
 }
